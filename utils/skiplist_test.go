c353497c9eca57350e022e046c9a2083bed92c8f
CoderfishGzh <335247945@qq.com>
2022-03-13 10:12:43 +0800

    finish skiplist

--- utils/skiplist_test.go
+++ utils/skiplist_test.go
@@ -48,16 +48,16 @@ func TestSkipListBasicCRUD(t *testing.T) {
 	//Put & Get
 	entry1 := codec.NewEntry([]byte("Key1"), []byte("Val1"))
 	assert.Nil(t, list.Add(entry1))
+	assert.NotNil(t, list.Search(entry1.Key))
 	assert.Equal(t, entry1.Value, list.Search(entry1.Key).Value)
 
 	entry2 := codec.NewEntry([]byte("Key2"), []byte("Val2"))
 	assert.Nil(t, list.Add(entry2))
 	assert.Equal(t, entry2.Value, list.Search(entry2.Key).Value)
 
-	//Get a not exist entry
+	////Get a not exist entry
 	assert.Nil(t, list.Search([]byte("noexist")))
-
-	//Update a entry
+	////Update a entry
 	entry2_new := codec.NewEntry([]byte("Key1"), []byte("Val1+1"))
 	assert.Nil(t, list.Add(entry2_new))
 	assert.Equal(t, entry2_new.Value, list.Search(entry2_new.Key).Value)
