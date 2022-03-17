acbe596b4012a6f5aee0eb3f4e93e16c316c5ef6
CoderfishGzh <335247945@qq.com>
2022-03-17 20:15:31 +0800

    add arean Explanation

--- utils/skiplist_test.go
+++ utils/skiplist_test.go
@@ -23,15 +23,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func RandString(len int) string {
-	bytes := make([]byte, len)
-	for i := 0; i < len; i++ {
-		b := r.Intn(26) + 65
-		bytes[i] = byte(b)
-	}
-	return string(bytes)
-}
-
 func TestSkipListBasicCRUD(t *testing.T) {
 	list := NewSkipList(1000)
 
@@ -53,10 +44,19 @@ func TestSkipListBasicCRUD(t *testing.T) {
 	assert.Equal(t, entry2_new.Value, list.Search(entry2_new.Key).Value)
 }
 
+//当申请的zrenaSize为1000， 但是add的操作次数为1000000时
+//会发生panic，why？
+//原因：不断向skiplist插入数据，而arena不断申请内存
+//当申请的内存超过预设的大小时，缺没有flush到sst的操作，所以会出错
+//关于SST的相关知识，在lession5有进行讲解
 func Benchmark_SkipListBasicCRUD(b *testing.B) {
 	list := NewSkipList(1000)
-	key, val := "", ""
 	maxTime := 1000000
+	key, val := "", ""
+
+	//list := NewSkipList(100000)
+	//maxTime := 1000
+
 	for i := 0; i < maxTime; i++ {
 		//number := rand.Intn(10000)
 		key, val = fmt.Sprintf("Key%d", i), fmt.Sprintf("Val%d", i)
